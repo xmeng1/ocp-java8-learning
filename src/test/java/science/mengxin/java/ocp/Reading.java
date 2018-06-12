f84c2095bbc21a7416a624a65a51e31df57c8ceb
Xin Meng <x.meng@outlook.com>
2018-06-12 08:55:59 +0100

    map filter reduce

---
+++ src/test/java/science/mengxin/java/ocp/Reading.java
@@ -0,0 +1,15 @@
+package science.mengxin.java.ocp;
+
+public class Reading {
+    int year;
+    int month;
+    int day;
+    double value;
+
+    public Reading(int year, int month, int day, double value) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.value = value;
+    }
+}
--- src/test/java/science/mengxin/java/ocp/SteamsTest.java
+++ src/test/java/science/mengxin/java/ocp/SteamsTest.java
@@ -1,12 +1,32 @@
 package science.mengxin.java.ocp;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 public class SteamsTest {
+
+    List<Reading> readings;
+
+
+    @BeforeEach
+    void setUp() {
+        readings = Arrays.asList(
+                new Reading(2017, 1, 1, 405.91),
+                new Reading(2017, 1, 8, 405.98),
+                new Reading(2017, 1, 15, 406.14),
+                new Reading(2017, 1, 22, 406.48),
+                new Reading(2017, 1, 29, 406.20),
+                new Reading(2017, 2, 5, 407.12),
+                new Reading(2017, 2, 12, 406.03)
+        );
+    }
 
     @Test
     void helloWorld() {
@@ -30,7 +50,34 @@ public class SteamsTest {
 
         long x = tempsInPhoenix.stream().filter(t -> t > 100).count();
         System.out.println(x);
+    }
 
+
+    @Test
+    void mapFilterReduceTest() {
+        OptionalDouble optionalDouble = readings.stream().mapToDouble(r -> r.value)
+                .filter(v -> v >= 406.0 && v < 407.00).average();
+
+        if (optionalDouble.isPresent()) {
+            System.out.println(optionalDouble.getAsDouble());
+        }
+
+        Optional<Double> sum = readings.stream().map(r -> r.value).reduce((v1, v2) -> v1 + v2);
+
+        if (sum.isPresent()) {
+            System.out.println("sum is: " + sum);
+        }
+
+        // reduce will do lambda for all item in steam and put last return as first input
+        Optional<Double> sum2 =  readings.stream().map(r -> r.value).reduce((v1, v2) -> {
+            System.out.println("v1:" + v1);
+            System.out.println("v2:" + v2);
+            return 1.000;
+        });
+
+        if (sum.isPresent()) {
+            System.out.println("sum is: " + sum2);
+        }
 
     }
 }
